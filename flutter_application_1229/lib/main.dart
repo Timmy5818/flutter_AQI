597ee14e7f71c722265dd1e7e5435afa5469ecfa
Timmy5818 <qazwsxedc12341@hotmail.com>
2022-01-05 19:49:24 +0800

    first commit

---
+++ flutter_application_1229/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'api_server.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(),
+        body: AQIList(),
+      ),
+    );
+  }
+}
+
+class AQIList extends StatefulWidget {
+  const AQIList({Key? key}) : super(key: key);
+  @override
+  _AQIListState createState() => _AQIListState();
+}
+
+class _AQIListState extends State<AQIList> {
+  Future<List<Station>>? futureStationList;
+
+  @override
+  void initState() {
+    super.initState();
+    futureStationList = fetchAQI();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder<List<Station>>(
+        future: futureStationList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            print(snapshot.data);
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                print('build ${index}');
+                return ListTile(
+                  title: Text(snapshot.data![index].siteName),
+                  subtitle: Text(snapshot.data![index].aqi.toString()),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
